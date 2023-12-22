d331b97128f903ee905c2ac2813a33fd20b06b50
forteleaf <forteleaf@hotmail.com>
2023-12-23 02:33:02 +0900

    add counter.ex

--- lib/counter.ex
+++ lib/counter.ex
@@ -6,4 +6,30 @@ defmodule Counter do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  use CounterWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, :val, 0)}
+  end
+
+  def handle_event("inc", _, socket) do
+    {:noreply, update(socket, :val, &(&1 + 1))}
+  end
+
+  def handle_event("dev", _, socket) do
+    {:noreply, update(socket, :val, &(&1 - 1))}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <h1 class="teext-4xl font-bold text-center">The count is: <%= @val %></h1>
+
+      <p class="text-center">
+        <.button phx-click="dec">-</.button>
+        <.button phx-click="inc">+</.button>
+      </p>
+    </div>
+    """
+  end
 end
