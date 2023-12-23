4cb15ec8042fe2654e33ad74d6a4be50edde11bc
forteleaf <forteleaf@hotmail.com>
2023-12-24 00:40:21 +0900

    feature: share state between clients

--- lib/counter.ex
+++ lib/counter.ex
@@ -9,37 +9,45 @@ defmodule CounterWeb.Counter do
   if it comes from the database, an external API or others.
   """
   use CounterWeb, :live_view
+  @topic "live"
   # CounterWeb 모듈에서 :live_view 관련 기능을 가져와 사용
 
   # mount 함수는 LiveView가 마운트될 때 호출됩니다.
   # 초기 상태를 설정하는 데 사용되며,
   # 여기서는 소켓에 :val이라는 이름으로 0을 할당합니다.
   def mount(_params, _session, socket) do
+    # subscribe
+    CounterWeb.Endpoint.subscribe(@topic)
     {:ok, assign(socket, :val, 0)}
   end
 
   # handle_event 함수는 특정 이벤트를 처리합니다.
   # "inc" 이벤트가 발생하면 :val 값을 1 증가시킵니다.
-  def handle_event("inc", _, socket) do
-    {:noreply, update(socket, :val, &(&1 + 1))}
+  def handle_event("inc", _value, socket) do
+    new_state = update(socket, :val, &(&1 + 1))
+    CounterWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
+    {:noreply, new_state}
   end
 
   # "dec" 이벤트에 대해서는 :val 값을 1
-  def handle_event("dev", _, socket) do
+  def handle_event("dec", _, socket) do
+    new_state = update(socket, :val, &(&1 - 1))
+    CounterWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
     {:noreply, update(socket, :val, &(&1 - 1))}
+  end
+
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, val: msg.payload.val)}
   end
 
   # render 함수는 LiveView의 HTML을 렌더링합니다.
   # ~H 시그릴은 HTML을 Elixir 코드 내에서 사용할 수 있습니다.
   def render(assigns) do
     ~H"""
-    <div>
-      <h1 class="teext-4xl font-bold text-center">The count is: <%= @val %></h1>
-
-      <p class="text-center">
-        <.button phx-click="dec">-</.button>
-        <.button phx-click="inc">+</.button>
-      </p>
+    <div class="text-center">
+      <h1 class="teext-4xl font-bold text-center">Count: <%= @val %></h1>
+      <.button phx-click="dec">-</.button>
+      <.button phx-click="inc">+</.button>
     </div>
     """
   end
