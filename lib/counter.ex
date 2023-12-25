7d137a2dfce49314d71374b308eb477e055a4188
forteleaf <forteleaf@hotmail.com>
2023-12-25 09:00:00 +0900

    chore: desc comments

--- lib/counter.ex
+++ lib/counter.ex
@@ -16,16 +16,30 @@ defmodule CounterWeb.Counter do
   # 초기 상태를 설정하는 데 사용되며,
   # 여기서는 소켓에 :val이라는 이름으로 0을 할당합니다.
   def mount(_params, _session, socket) do
-    # subscribe
+    # 설명
+    # 특정 토픽에 대한 구독을 설정합니다.
     CounterWeb.Endpoint.subscribe(@topic)
+
+    # 상태를 설정합니다.
+    # :val 이라는 키를 가진 상태를 할당하고, 그 값으로 0을 할당합니다.
+
     {:ok, assign(socket, :val, 0)}
   end
 
   # handle_event 함수는 특정 이벤트를 처리합니다.
   # "inc" 이벤트가 발생하면 :val 값을 1 증가시킵니다.
+  # CounterWeb.Endpoint.broadcast_from/4 함수를 사용하여
+  # 현재 프로세스(self())에서 @topic이라는 주제로
+  # "inc" 이벤트와 함께 새로운 상태(new_state.assigns)를 브로드캐스트합니다.
+  # 이를 통해 같은 토픽을 구독하는 다른 클라이언트나 서버 사이드 프로세스에게
+  # 이벤트를 알립니다.
   def handle_event("inc", _value, socket) do
     new_state = update(socket, :val, &(&1 + 1))
     CounterWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
+
+    # {:noreply, new_state}를 반환하여,
+    # 이벤트에 대한 응답으로 추가적인 메시지를 보내지 않고,
+    # 소켓의 상태를 new_state로 업데이트
     {:noreply, new_state}
   end
 
@@ -36,6 +50,12 @@ defmodule CounterWeb.Counter do
     {:noreply, update(socket, :val, &(&1 - 1))}
   end
 
+  @doc """
+  handler_info/2는 msg가 수신된 메시지이고,
+  소켓이 Phoenix.Socket인 Elixir 프로세스 메시지를 처리합니다.
+  마지막으로 우리는 좀 더 시각적으로 매력적이도록,
+  render/1 함수 내부의 HTML을 수정했습니다.
+  """
   def handle_info(msg, socket) do
     {:noreply, assign(socket, val: msg.payload.val)}
   end
